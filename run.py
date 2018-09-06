b98e0d8d70436df80588da9c068ab98c3c952835
Jason Street <jason.street@learningpeople.co.uk>
2018-09-06 12:49:14 +0000

    add more advanced functionality

--- run.py
+++ run.py
@@ -1,28 +1,36 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
     """Add messages to the saved list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    message_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(message_dict)
 
 
 def get_all_messages():
     """Get all of the messages and separate with a break"""
-    return "<br>".join(messages)
+    return messages
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
     
     
 @app.route('/<username>')
 def user(username):
     """display chat messages"""
-    return "<h2>Welcome, {0}</h2>{1}".format(username, get_all_messages())
+    messages = get_all_messages()
+    return render_template("chat.html", username=username, chat_messages=messages)
     
 
 @app.route('/<username>/<message>')
