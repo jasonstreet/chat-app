ebbc1edad474bc7805799848bbc904d881c09fff
Jason Street <jason.street@learningpeople.co.uk>
2018-09-06 13:52:54 +0000

    Update 6/09/2018

--- run.py
+++ run.py
@@ -3,25 +3,34 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-messages = []
+
+def write_to_file(filename, data):
+    """Handle writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
 
 def add_messages(username, message):
     """Add messages to the saved list"""
-    now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(messages_dict)
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"), 
+            username.title(), 
+            message))
 
 
 def get_all_messages():
     """Get all of the messages and separate with a break"""
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
     return messages
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
+    
+    #Post request
     if request.method == "POST":
-        with open("data/users.txt", "a") as user_list:
-            user_list.writelines(request.form["username"] + "\n")
+        write_to_file("data/users.txt", request.form["username"] + "\n")
         return redirect(request.form["username"])
     return render_template("index.html")
     
